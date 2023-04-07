161867396b10bb7a5ad4834b25842414cea56a80
Vinícius Marcili <vinipetter@gmail.com>
2023-04-07 10:03:40 -0300

    :tada:

---
+++ wordfence-waf.php
@@ -0,0 +1,11 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+// This file was the current value of auto_prepend_file during the Wordfence WAF installation (Thu, 06 Apr 2023 14:36:29 +0000)
+if (file_exists('/home/wt1co931/public_html/wordfence-waf.php')) {
+	include_once '/home/wt1co931/public_html/wordfence-waf.php';
+}
+if (file_exists(__DIR__.'/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", __DIR__.'/wp-content/wflogs/');
+	include_once __DIR__.'/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
