87dc341b1f078923f28ca679ee09ffacbc572daa
Vinícius Marcili <vinipetter@gmail.com>
2023-04-07 18:09:39 -0300

    Layout adjustments

--- wp-content/themes/twentytwentythree/page-login.php
+++ wp-content/themes/twentytwentythree/page-login.php
@@ -3,10 +3,17 @@
 <div class='wrapper' style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/img/bg.png');">
   <div class='login' id='login-wrapper'>
     <header>
-      <figure class='logo-login mobile'>
+      <figure class='header'>
         <img
           class='img-fluid'
-          src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png"
+          src="<?php echo get_template_directory_uri() ?>/assets/img/header.png"
+          alt="Logo"
+        />
+      </figure>
+      <!-- <figure class='logo-login mobile'>
+        <img
+          class='img-fluid'
+          src="<?php //echo get_template_directory_uri() ?>/assets/img/logo.png"
           alt="Logo"
         />
       </figure>
@@ -16,26 +23,26 @@
       <figure class='logo-login desktop'>
         <img
           class='img-fluid'
-          src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png"
+          src="<?php //echo get_template_directory_uri() ?>/assets/img/logo.png"
           alt="Logo"
         />
-      </figure>
+      </figure> -->
     </header>
     <main>
       <figure>
         <img
           class='img-fluid'
+          style="max-width: 780px; width: 100%;"
           src="<?php echo get_template_directory_uri() ?>/assets/img/initial.png"
           alt="Juntos para Somar"
         />
       </figure>
       <form onsubmit="login()">
         <div class='form-group'>
-          <label for='email'>E-mail</label>
-          <input type='email' class='form-control' id='email' placeholder='email@exemplo.com' required />
+          <input type='email' class='form-control' id='email' placeholder='E-mail' required style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/img/bg-input.png');" />
         </div>
         <div class='form-group'>
-          <button type='submit' class='button'>
+          <button type='submit' class='button-orange' style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/img/bg-input.png');">
             Entrar
           </button>
         </div>
@@ -51,7 +58,7 @@
           alt="Logo"
         />
       </figure>
-      <button type='button' class='button' onclick="logout()">
+      <button type='button' class='button-orange' onclick="logout()">
         Sair
       </button>
     </header>
@@ -59,7 +66,7 @@
       <div class='container'>
         <iframe class='video-iframe' src="https://vimeo.com/event/3252107/embed/047a27bfb6" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <iframe class='chat-iframe' src="https://vimeo.com/event/3252107/chat/047a27bfb6" width="100%" height="100%" style='min-height: 350px' frameborder="0"></iframe>
+      <iframe class='chat-iframe' src="https://vimeo.com/event/3252107/chat/047a27bfb6" width="100%" style='min-height: 350px' frameborder="0"></iframe>
     </main>
   </div>
 </div>
