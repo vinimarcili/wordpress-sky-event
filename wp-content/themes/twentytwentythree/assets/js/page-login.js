5b2599ed3e5c6680afd401a5f93665849e467768
Vinícius Marcili <vinipetter@gmail.com>
2023-04-07 15:27:03 -0300

    Finish

--- wp-content/themes/twentytwentythree/assets/js/page-login.js
+++ wp-content/themes/twentytwentythree/assets/js/page-login.js
@@ -1,27 +1,47 @@
+SPREADSHEET_URL = '/assets/emails.csv'
+
 DOMAINS = [
   'sky.com.br'
 ]
 
-function login() {
-  event.preventDefault();
-  const email = document.getElementById('email').value
+async function login() {
+  event.preventDefault()
+  Toast.cancelAll()
+  const themeUrl = document.getElementById('template-uri').innerHTML
+  const email = document.getElementById('email').value.trim()
   const login = document.getElementById('login-wrapper')
   const video = document.getElementById('video-wrapper')
 
+  // read csv
+  const csv = new XMLHttpRequest()
+  csv.open('GET', themeUrl + SPREADSHEET_URL, false)
+  csv.send()
+  csv.responseText.replaceAll('\r', '')
+  const lines = csv.responseText.split('\n')
+  const emails = lines.map(line => line.split(',')[0]).map(email => email.trim())
+  // remove first line
+  emails.shift()
+  const isValidEmail = emails.includes(email)
   const isValidDomain = DOMAINS.includes(email.split('@')[1])
 
-  if (!isValidDomain) {
+  if (!isValidEmail && !isValidDomain) {
     Toast.error('E-mail não autorizado', {
       closeButton: true,
       className: 'full',
       position: `bottom center`,
-      duration: 360000,
+      duration: 30000,
       notOverClick: true,
       immediately: true,
     })
     return
   }
-  
+  login.classList.toggle('hidden')
+  video.classList.toggle('hidden')
+}
+
+async function logout() {
+  const login = document.getElementById('login-wrapper')
+  const video = document.getElementById('video-wrapper')
   login.classList.toggle('hidden')
   video.classList.toggle('hidden')
 }
--- wp-content/themes/twentytwentythree/page-login.php
+++ wp-content/themes/twentytwentythree/page-login.php
@@ -1,4 +1,5 @@
 <?php get_header() ?>
+<div id='template-uri' style='display:none;'><?php echo get_template_directory_uri() ?></div>
 <div class='wrapper' style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/img/bg.png');">
   <div class='login' id='login-wrapper'>
     <header>
@@ -31,10 +32,10 @@
       <form onsubmit="login()">
         <div class='form-group'>
           <label for='email'>E-mail</label>
-          <input type='email' class='form-control' id='email' placeholder='email@exemplo.com' />
+          <input type='email' class='form-control' id='email' placeholder='email@exemplo.com' required />
         </div>
         <div class='form-group'>
-          <button type='submit' class='btn btn-primary'>
+          <button type='submit' class='button'>
             Entrar
           </button>
         </div>
@@ -50,12 +51,15 @@
           alt="Logo"
         />
       </figure>
+      <button type='button' class='button' onclick="logout()">
+        Sair
+      </button>
     </header>
     <main>
       <div class='container'>
         <iframe class='video-iframe' src="https://vimeo.com/event/3252107/embed/047a27bfb6" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <iframe class='chat-iframe' src="https://vimeo.com/event/3252107/chat/047a27bfb6" width="100%" height="350px" frameborder="0"></iframe>
+      <iframe class='chat-iframe' src="https://vimeo.com/event/3252107/chat/047a27bfb6" width="100%" height="100%" style='min-height: 350px' frameborder="0"></iframe>
     </main>
   </div>
 </div>
